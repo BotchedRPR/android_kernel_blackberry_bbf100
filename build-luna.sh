44aa4ecffe778d464050e49dee9566114c7319cb
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2025-04-09 13:50:21 +0200

    [DO NOT MERGE] buildscripts: use the new defconfig for luna
    Signed-off-by: Igor Belwon <igor.belwon@mentallysanemainliners.org>

--- build-luna.sh
+++ build-luna.sh
@@ -14,8 +14,8 @@ export CROSS_COMPILE=/media/igor/Android/bbry/aarch64-linux-android-4.9/bin/aarc
 
 #make O=../BUILD CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH mrproper
 
-# use config extracted from device for now
-make O=../BUILD CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH CONFIG_SUNWAVE_FINGERPRINT=y -j12 Image.gz
+make O=../BUILD CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH luna-perf_defconfig
+make O=../BUILD CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH -j12 Image.gz
 cat ../BUILD/arch/arm64/boot/Image.gz '/media/igor/Android/bbry/android-linux-kernel/bimg/extractions/kernel.extracted/A80FC0/dtb1' '/media/igor/Android/bbry/android-linux-kernel/bimg/extractions/kernel.extracted/A80FC0/dtb2' '/media/igor/Android/bbry/android-linux-kernel/bimg/extractions/kernel.extracted/A80FC0/dtb3' > Image.gz-dtb
 mkbootimg --header_version 0 --os_patch_level 2020-11 --os_version 8.1.0 --pagesize 4096 --tags_offset 0x100 --second_offset 0x00f00000 --ramdisk_offset 0x01000000 --kernel_offset 0x00008000 --base 0x0 --cmdline "earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 pathtrust=0 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 build_number=ACT575 androidboot.build_number=ACT575 coherent_pool=1280K buildvariant=user" --kernel Image.gz-dtb --ramdisk '/media/igor/Android/bbry/recovery/ramdisk'   --second '/media/igor/Android/bbry/android-linux-kernel/bimg/second'  --out newbootimg
 
